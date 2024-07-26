eafd6b91c9cb16df034993f7c3f8900cb272a572
kitifi0 <williamskitisha@gmail.com>
2024-07-26 10:31:09 -0500

    Update Friday.py

--- Friday.py
+++ Friday.py
@@ -1,16 +1,11 @@
-!npm install localtunnel 
-%%writefile app.py
-  
-%%writefile app.py
-
-import streamlit as st
+import streamlit as st
 
 # Set the title of the app
 st.title("Welcome to My First Streamlit App")
 
 # Add a text input
-name = st.text_input("Enter your name:")
+name = st.text_input("Enter your name:")
 
 # Display the name entered by the user
-if name:
-  st.write(f"Hello, {name}! Welcome to the app.")
+if name:
+    st.write(f"Hello, {name}! Welcome to the app.")
